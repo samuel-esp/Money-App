bb53e77cb130fc9168745dd1c325e52ba9e0d466
Samuel Esposito <samuel-esp@hotmail.it>
2019-06-27 00:17:11 +0200

    Initial Commit

---
+++ Money AppTests/Money_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Money_AppTests.swift
+//  Money AppTests
+//
+//  Created by Samuel-Mac on 27/06/2019.
+//  Copyright © 2019 Samuel Esposito. All rights reserved.
+//
+
+import XCTest
+@testable import Money_App
+
+class Money_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
